748c6f086f019f69676d2b9fb64354be73bc8b76
Ahmed Mohamed <ahmedmohamed00521@gmail.com>
2025-09-23 19:23:11 +0300

    updateuserDto

--- Backend/MasarSkills.API/DTOs/UserDto.cs
+++ Backend/MasarSkills.API/DTOs/UserDto.cs
@@ -3,6 +3,8 @@
     public class UserDto
     {
         public int Id { get; set; }
+        public string FirstName { get; set; }    
+        public string LastName { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
         public string Role { get; set; }
